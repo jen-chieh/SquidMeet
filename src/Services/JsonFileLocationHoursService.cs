88c2a10d31811a700bcb48dfee884c650ab1189f
Selina Hu <Selinaehu@gmail.com>
2022-04-19 16:07:50 -0700

    Added comments to JsonFileLocationHoursService.cs to describe class and attributes.

--- src/Services/JsonFileLocationHoursService.cs
+++ src/Services/JsonFileLocationHoursService.cs
@@ -7,18 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
+    /// <summary>
+    /// Create class from location_hours.json file and retrieve all data from file.
+    /// </summary>
     public class JsonFileLocationHoursService
     {
+        // Initialize class
         public JsonFileLocationHoursService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
+        // Data middle tier
         public IWebHostEnvironment WebHostEnvironment { get; }
 
-        // Get json path 
+        // Get json file path 
         private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "location_hours.json");
 
+        // Read .json file and return all data fields through the model
         public IEnumerable<LocationHoursModel> GetLocationHours()
         {
             using var jsonFileReader = File.OpenText(JsonFileName);
