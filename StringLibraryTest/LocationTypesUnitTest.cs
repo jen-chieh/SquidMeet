aa388ecb77c10fa48e5136fcf5a12655c279acb1
Selina Hu <Selinaehu@gmail.com>
2022-04-19 22:57:18 -0700

    Added unit test outline for location_types object (2 tests).

---
+++ StringLibraryTest/LocationTypesUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using ContosoCrafts.WebSite.Controllers;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringLibraryTest
+{
+    /// <summary>
+    /// Unit test for the location_types object/model that tests getting the correct value
+    /// and if there are only three different location types.
+    /// </summary>
+    [TestClass]
+    public class LocationTypesUnitTest
+    {
+        [TestMethod]
+        public void TestGetCorrectValue()
+        {
+            // Implement test function to test if the correct value is returned for a data field
+        }
+
+        [TestMethod]
+        public void TestNumValues()
+        {
+            // Implement test function to test if there are only 3 location types total
+        }
+
+    }
+}
