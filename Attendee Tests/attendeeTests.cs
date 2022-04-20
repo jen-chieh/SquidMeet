e0d5e728261b968f467dc4d08040d54334cb62e5
Youjin <leeyucong@yahoo.com>
2022-04-19 22:33:12 -0700

    Added unit test for Attendee.cs

---
+++ Attendee Tests/attendeeTests.cs
@@ -0,0 +1,29 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Xunit;
+
+namespace Attendee_Tests
+{
+    public class attendeeTests
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            // Arrange
+            var attendee = new Attendee();
+
+            // Act
+            
+            
+            // Assert
+            Assert.NotNull(attendee);
+            Assert.Null(attendee.Event);
+            Assert.Null(attendee.EventId);
+            Assert.Null(attendee.User);
+            Assert.Null(attendee.UserId);
+       
+        }
+      
+    }
+   
+}
