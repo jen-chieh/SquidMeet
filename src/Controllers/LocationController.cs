5f8bf4bd828f4ec11c1b7830287bd0f052ff6236
Selina Hu <Selinaehu@gmail.com>
2022-04-24 15:58:13 -0700

    Added comments to LocationController.cs file.

--- src/Controllers/LocationController.cs
+++ src/Controllers/LocationController.cs
@@ -12,14 +12,36 @@ namespace ContosoCrafts.WebSite.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    /// <summary>
+    /// Controller class for location.json file object with support for adding ratings
+    /// </summary>
     public class LocationController : Controller
     {
+        // Initialize controller
         public LocationController(JsonFileLocationService locationService) =>
             LocationService = locationService;
 
+        // Get location service
         public JsonFileLocationService LocationService { get; }
 
+        // Get information from location.json file
         [HttpGet]
         public IEnumerable<LocationModel> Get() => LocationService.GetLocations();
+
+        // Response to rating request
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            LocationService.AddRating(request.location_id, request.Rating);
+
+            return Ok();
+        }
+
+        // New rating to be added for a specific location_id
+        public class RatingRequest
+        {
+            public string location_id { get; set; }
+            public int Rating { get; set; }
+        }
     }
 }
