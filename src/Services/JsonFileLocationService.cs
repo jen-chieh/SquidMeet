a5bb967065a916a7579e85b1dce9a09b6be420c5
Selina Hu <Selinaehu@gmail.com>
2022-04-24 16:58:45 -0700

    Changed name of JsonFileProductService.cs to JsonFileLocationService.cs.

--- src/Services/JsonFileLocationService.cs
+++ src/Services/JsonFileLocationService.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+
 using ContosoCrafts.WebSite.Models;
+
 using Microsoft.AspNetCore.Hosting;
+
 namespace ContosoCrafts.WebSite.Services
 {
-    /// <summary>
-    /// Service for location.json file to fetch data from database
-    /// </summary>
-    public class JsonFileLocationService
+   public class JsonFileLocationService
     {
         public JsonFileLocationService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
+
         public IWebHostEnvironment WebHostEnvironment { get; }
 
-        // Get json path 
-        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "location.json");
-
-        public IEnumerable<LocationModel> GetLocations()
+        private string JsonFileName
         {
-            using var jsonFileReader = File.OpenText(JsonFileName);
-
-            return JsonSerializer.Deserialize<LocationModel[]>(jsonFileReader.ReadToEnd(),
-                new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
-
-
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "location.json"); }
         }
 
-        // Method to add a user rating to location
-        public void AddRating(string locationId, int rating)
+        public IEnumerable<LocationModel> GetAllData()
         {
-            var locations = GetLocations();
-
-            if (locations.First(x => x.location_id == locationId).rating == null)
+            using(var jsonFileReader = File.OpenText(JsonFileName))
             {
-                locations.First(x => x.location_id == locationId).rating = new int[] { rating };
+                return JsonSerializer.Deserialize<LocationModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetAllData();
+
+            if(products.First(x => x.location_id == productId).rating == null)
+            {
+                products.First(x => x.location_id == productId).rating = new int[] { rating };
             }
             else
             {
-                var ratings = locations.First(x => x.location_id == locationId).rating.ToList();
+                var ratings = products.First(x => x.location_id == productId).rating.ToList();
                 ratings.Add(rating);
-                locations.First(x => x.location_id == locationId).rating = ratings.ToArray();
+                products.First(x => x.location_id == productId).rating = ratings.ToArray();
             }
 
-            using (var outputStream = File.OpenWrite(JsonFileName))
+            using(var outputStream = File.OpenWrite(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<LocationModel>>(
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
                     {
                         SkipValidation = true,
                         Indented = true
-                    }),
-                    locations
+                    }), 
+                    products
                 );
             }
         }
@@ -71,27 +71,27 @@ namespace ContosoCrafts.WebSite.Services
         /// <param name="data"></param>
         public LocationModel UpdateData(LocationModel data)
         {
-            var locations = GetLocations();
-            var locationData = locations.FirstOrDefault(x => x.location_id.Equals(data.location_id));
-            if (locationData == null)
+            var products = GetAllData();
+            var productData = products.FirstOrDefault(x => x.location_id.Equals(data.location_id));
+            if (productData == null)
             {
                 return null;
             }
 
-            locationData.name = data.name;
-            locationData.type_id = data.type_id;
-            locationData.address = data.address;
-            locationData.Image = data.Image;
+            productData.name = data.name;
+            productData.type_id = data.type_id;
+            productData.address = data.address;
+            productData.Image = data.Image;
 
-            SaveData(locations);
+            SaveData(products);
 
-            return locationData;
+            return productData;
         }
 
         /// <summary>
-        /// Save All location data to storage
+        /// Save All products data to storage
         /// </summary>
-        private void SaveData(IEnumerable<LocationModel> locations)
+        private void SaveData(IEnumerable<LocationModel> products)
         {
 
             using (var outputStream = File.Create(JsonFileName))
@@ -102,15 +102,14 @@ namespace ContosoCrafts.WebSite.Services
                         SkipValidation = true,
                         Indented = true
                     }),
-                    locations
+                    products
                 );
             }
         }
 
         /// <summary>
-        /// Create a new location using default values
+        /// Create a new product using default values
         /// After create the user can update to set values
-        /// Type_id set to 1
         /// </summary>
         /// <returns></returns>
         public LocationModel CreateData()
@@ -124,8 +123,8 @@ namespace ContosoCrafts.WebSite.Services
                 Image = "",
             };
 
-            // Get the current set, and append the new record to it because IEnumerable does not have Add function
-            var dataSet = GetLocations();
+            // Get the current set, and append the new record to it
+            var dataSet = GetAllData();
             dataSet = dataSet.Append(data);
 
             SaveData(dataSet);
@@ -140,16 +139,15 @@ namespace ContosoCrafts.WebSite.Services
         public LocationModel DeleteData(string id)
         {
             // Get the current set, and append the new record to it
-            var dataSet = GetLocations();
+            var dataSet = GetAllData();
             var data = dataSet.FirstOrDefault(m => m.location_id.Equals(id));
 
-            var newDataSet = GetLocations().Where(m => m.location_id.Equals(id) == false);
-
+            var newDataSet = GetAllData().Where(m => m.location_id.Equals(id) == false);
+            
             SaveData(newDataSet);
 
             return data;
         }
-
-    
+        
     }
 }
