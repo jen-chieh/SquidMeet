a7fe1cdd5d63903d5886981256cbe51ea07e5778
james <x0930186031@gmail.com>
2022-04-25 16:24:32 -0700

    Added comment above each variable to LocationModel

--- src/Models/LocationModel.cs
+++ src/Models/LocationModel.cs
@@ -5,13 +5,18 @@ namespace ContosoCrafts.WebSite.Models
 {
     public class LocationModel
     {
+        //Unique Id assign to location
         public string? location_id { get; set; }
+        // Name of the location
         public string? name { get; set; }
+        // the type of location Id to connect locationType.json 
         public string type_id { get; set; }
+        // Address of Location 
         public string? address { get; set; }
         [JsonPropertyName("img")]
+        //Image of location 
         public string? Image { get; set; }
-
+        // rating for Location for sorting and filtering.
         public int[]? rating { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<LocationModel>(this);
