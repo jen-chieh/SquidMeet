e3aa1f460379e56c7e9dc6fc585fc54f9dc739a8
Selina Hu <Selinaehu@gmail.com>
2022-04-24 23:27:10 -0700

    Changed LocationType.cs model name to LocationTypeModel.cs.

---
+++ src/Models/LocationTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class LocationTypeModel
+    {
+        
+        public string? type_id { get; set; }
+        public string? type_name { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<LocationTypeModel>(this);
+    }
+}
