7d339488a52bb0b0dbf31073e9fc342d4dbc9425
james <x0930186031@gmail.com>
2022-04-25 16:24:15 -0700

    Added comment above each variable to LocationTypeModel

--- src/Models/LocationTypeModel.cs
+++ src/Models/LocationTypeModel.cs
@@ -6,8 +6,10 @@ namespace ContosoCrafts.WebSite.Models
 {
     public class LocationTypeModel
     {
-        
+
+        //Unique Id assigned to location type
         public string? type_id { get; set; }
+        // Name of location type
         public string? type_name { get; set; }
         public override string ToString() => JsonSerializer.Serialize<LocationTypeModel>(this);
     }
