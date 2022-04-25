11fb2ef1a79abd5fdf1a271abbc141d94d652e66
james <x0930186031@gmail.com>
2022-04-25 10:56:12 -0700

    Add SaveData and CreateUser function in JsonFileUserService

--- src/Services/JsonFileUserService.cs
+++ src/Services/JsonFileUserService.cs
@@ -35,6 +35,46 @@ namespace ContosoCrafts.WebSite.Services
                     PropertyNameCaseInsensitive = true
                 });        
         }
+        /// <summary>
+        /// Save All products data to storage
+        /// </summary>
+        private void SaveData(IEnumerable<UserModel> products)
+        {
 
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<UserModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+
+        public UserModel CreateUser()
+        {
+            var data = new UserModel()
+            {
+                user_id = GetHashCode(),
+                username = "Enter User Name",
+                password = "Enter password",
+                name = "Enter Name",
+                age = 0,
+                gender= "Enter Gender",
+                bio="Enter Bio"
+
+            };
+
+            // Get the current set, and append the new record to it
+            var dataSet = GetUsers();
+            dataSet = dataSet.Append(data);
+
+            SaveData(dataSet);
+
+            return data;
+        }
     }
 }
