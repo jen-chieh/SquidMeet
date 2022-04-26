cb172e9a8abd1f28ffda4f53bb84e69522fa5968
tkalyani <tkalyani@seattleu.edu>
2022-04-25 23:28:53 -0700

    added comments to program.cs file.

--- src/Program.cs
+++ src/Program.cs
@@ -5,9 +5,10 @@ namespace ContosoCrafts.WebSite
 {
     public class Program
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
         public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-
+        // This method gets called by the runtime. Use this method to configure the HTTP request's pipeline. 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
