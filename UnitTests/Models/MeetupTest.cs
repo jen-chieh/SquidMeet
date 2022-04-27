d8a46af1c781594acd486a6a76ed9c96fd00cc51
Youjin <leeyucong@yahoo.com>
2022-04-26 18:48:37 -0700

    Created MeetupTests

---
+++ UnitTests/Models/MeetupTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models
+{   
+    [TestFixture]
+    internal class MeetupTest
+    {
+        private Meetup _meetup;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _meetup = new Meetup();
+        }
+    }
+}
