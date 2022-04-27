5f32e95a183ac0d7820b8e5bff1cdd26a8260702
Youjin <leeyucong@yahoo.com>
2022-04-26 19:06:43 -0700

    Added GetMeetupId_Valid_Test_Reading_Should_Return_ExpectedValue to MeetupTest

--- UnitTests/Models/MeetupTest.cs
+++ UnitTests/Models/MeetupTest.cs
@@ -16,5 +16,18 @@ namespace UnitTests.Models
         {
             _meetup = new Meetup();
         }
+
+        [Test]
+        public void GetMeetupId_Valid_Test_Reading_Should_Return_ExpectedValue()
+        {
+            // Arrange
+
+            // Act
+            var expectedValue = "625f11684b4cc8608a5ad58e";
+
+            // Assert
+            _meetup.meetup_id = expectedValue;
+            Assert.AreEqual(expectedValue, _meetup.meetup_id);
+        }
     }
 }
