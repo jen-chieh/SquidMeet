7c73cdc16c75fab78f67d2ee5e9f7e808bdce8e0
Youjin <leeyucong@yahoo.com>
2022-04-26 18:53:03 -0700

    Created LocationTests

---
+++ UnitTests/Models/LocationTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models
+{   
+    [TestFixture]
+    internal class LocationTests
+    {
+        private LocationModel _locationModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _locationModel = new LocationModel();
+        }
+    }
+}
