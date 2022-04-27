44534195b6b6c95c27dc540139b450be64b947ba
Youjin <leeyucong@yahoo.com>
2022-04-26 18:46:13 -0700

    Created UserModelTests

---
+++ UnitTests/Models/UserModelTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models
+{   
+    [TestFixture]
+    internal class UserModelTests
+    {
+        private UserModel _user;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _user = new Attendee();
+        }
+    }
+}
