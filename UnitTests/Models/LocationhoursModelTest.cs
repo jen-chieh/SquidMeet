6533fc5f503c573daa32d233302f86d0df0a488e
tkalyani1997 <tkalyani@seattleu.edu>
2022-04-26 22:39:12 -0700

    Added unit test for location hours model

---
+++ UnitTests/Models/LocationhoursModelTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Models;
+
+
+
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    internal class LocationhoursModelTests
+    {
+        private LocationHoursModel _locationhoursModel;
+
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _locationhoursModel = new LocationHoursModel();
+        }
+
+
+
+
+        [Test]
+        public void GetLocationId_Valid_Test_Reading_Should_Return_ExpectedValue()
+        {
+            // Arrange
+
+
+
+
+            // Act
+            var expectedValue = "0";
+
+
+
+
+            // Assert
+            _locationhoursModel.location_hours_id = expectedValue;
+            Assert.AreEqual(expectedValue, _locationhoursModel.location_hours_id);
+
+
+
+
+        }
+    }
+}
