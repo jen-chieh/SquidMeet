3a2563ec4b5b48288a00ae71768da041a757606d
Youjin <leeyucong@yahoo.com>
2022-04-26 18:42:25 -0700

    Created AttendeeTests

---
+++ UnitTests/Models/AttendeeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models
+{   
+    [TestFixture]
+    internal class AttendeeTests
+    {
+        private Attendee _attendee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _attendee = new Attendee();
+        }
+    }
+}
