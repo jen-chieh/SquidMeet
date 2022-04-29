3e9381e6d3aa6306f0e42899127048f2738c9f37
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-28 23:19:55 -0700

    Added Comments to Index.cshtml.cs

--- src/Pages/NewGroup/Index.cshtml.cs
+++ src/Pages/NewGroup/Index.cshtml.cs
@@ -8,24 +8,32 @@ namespace SquidMeet.WebSite.Pages.NewGroup
 {
     public class CreateNewGroupModel : PageModel
     {
+        // Data middle tier  
         public JsonFileMeetupService MeetupService { get; }
         public JsonFileLocationService LocationService { get; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="meetupService"></param>
+        /// <param name="locationService"></param>
         public CreateNewGroupModel(JsonFileMeetupService meetupService, JsonFileLocationService locationService)
         {
             this.MeetupService = meetupService;
             this.LocationService = locationService;
         }
 
+        // Data model
         [BindProperty]
         public LocationModel selectedLocation { get; set; }
 
-
+        // On get
         public void OnGet(string Id)
         {
             selectedLocation = LocationService.GetAllData().FirstOrDefault(x => x.location_id == Id);
         }
 
+        // On post
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
