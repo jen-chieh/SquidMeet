08d47d69126dc3000ed724cf966b5e7f718e8d85
Selina Hu <Selinaehu@gmail.com>
2022-04-29 14:13:30 -0700

    Added CreateAccount.cshtml.Tests.cs with 100% code coverage.

---
+++ UnitTests/Pages/Product/CreateAccount.cshtml.Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+
+
+namespace UnitTests.Pages.Product.Create
+{
+    public class CreateAccountTests
+    {
+        #region TestSetup
+        public static CreateUserModelModel pageModel;
+
+
+        // Create new CreateUserModel
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new CreateUserModelModel(TestHelper.UserService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        // Create new user and update user count
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Should_Return_Users()
+        {
+
+            // Arrange
+            var oldCount = TestHelper.UserService.GetUsers().Count();
+
+            // Act
+            pageModel.OnPost();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(oldCount + 1, TestHelper.UserService.GetUsers().Count());
+        }
+        #endregion OnPost
+    }
+}
