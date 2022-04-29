beceda9f30d1e97072172bc48d80189efb16b086
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-28 23:08:49 -0700

    Added framework for new group to Index.cshtml.cs

--- src/Pages/NewGroup/Index.cshtml.cs
+++ src/Pages/NewGroup/Index.cshtml.cs
@@ -1,12 +1,41 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SquidMeet.WebSite.Pages.NewGroup
 {
-    public class IndexModel : PageModel
+    public class CreateNewGroupModel : PageModel
     {
-        public void OnGet()
+        public JsonFileMeetupService MeetupService { get; }
+        public JsonFileLocationService LocationService { get; }
+
+        public CreateNewGroupModel(JsonFileMeetupService meetupService, JsonFileLocationService locationService)
         {
+            this.MeetupService = meetupService;
+            this.LocationService = locationService;
+        }
+
+        [BindProperty]
+        public LocationModel selectedLocation { get; set; }
+
+
+        public void OnGet(string Id)
+        {
+            selectedLocation = LocationService.GetAllData().FirstOrDefault(x => x.location_id == Id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            LocationService.UpdateData(selectedLocation);
+
+            return RedirectToPage("../Index");
         }
     }
 }
