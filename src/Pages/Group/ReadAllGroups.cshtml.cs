cf7348cdb1580f46f731c94ec2449018115dca06
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-29 09:07:13 -0700

    Added ReadAllGroups razor page

---
+++ src/Pages/Group/ReadAllGroups.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Group
+{
+    public class ReadAllGroupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
