d6b5df8b333f77b39b9db4c12696f86c5e0c747b
Selina Hu <Selinaehu@gmail.com>
2022-04-30 13:48:00 -0700

    Removed second ProfileModel variable in Profile.cshtml.cs.

--- src/Pages/Product/Profile.cshtml.cs
+++ src/Pages/Product/Profile.cshtml.cs
@@ -24,41 +24,16 @@ namespace ContosoCrafts.WebSite.Pages.Product
 
         // The data to show
         public UserModel Product;
-        public UserModel Product2;
         /// <summary>
         /// REST Get request
         /// </summary>
         /// <param name="id"></param>
         public void OnGet(string id)
         {
-            Product = ProductService.GetUsers().FirstOrDefault(m => m.username.Equals(id));
+            Product = ProductService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
 
         }
 
-        /*
-        // Data middle tier for location hours
-        public JsonFileLocationHoursService LocationHoursService { get; }
-
-        /// <summary>
-        /// Defualt Construtor for location hours as parameter
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="locationHoursService"></param>
-        public ReadModel(JsonFileLocationHoursService locationHoursService)
-        {
-            LocationHoursService = locationHoursService;
-        }
-
-        // The data to show for location hours
-        public LocationHoursModel LocationHours;
-
-        /// <summary>
-        /// REST Get request for location hours
-        /// </summary>
-        /// <param name="id"></param>
-        public void OnGetLocationHours(string id)
-        {
-            LocationHours = LocationHoursService.GetLocationHours().FirstOrDefault(m => m.location_hours_id.Equals(id));
-        }*/
+        
     }
 }
