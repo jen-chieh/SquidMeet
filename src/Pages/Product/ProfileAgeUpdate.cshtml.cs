1c944050d42788c27995d1e48fe50e7968fc5989
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-30 15:12:13 -0700

    Added separate page for updating user age

---
+++ src/Pages/Product/ProfileAgeUpdate.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Product
+{
+    public class ProfileAgeUpdateModel : PageModel
+    {
+        public ProfileAgeUpdateModel(JsonFileUserService userService)
+        {
+            UserService = userService;
+        }
+        public JsonFileUserService UserService { get; }
+
+        [BindProperty]
+        public UserModel UserProfile { get; set; }
+
+        public void OnGet(string id)
+        {
+            UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            UserService.UpdateUserAge(UserProfile);
+
+            return RedirectToPage("../Index");
+        }
+    }
+}
