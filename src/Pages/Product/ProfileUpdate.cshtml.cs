c33cef114221982a436c64d8fb8f1572329c41a6
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-30 11:34:14 -0700

    Created ProfileUpdate page

---
+++ src/Pages/Product/ProfileUpdate.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace SquidMeet.WebSite.Pages.Product
+{
+    public class ProfileUpdateModel : PageModel
+    {
+        public JsonFileUserService UserService { get; }
+
+        public ProfileUpdateModel(JsonFileUserService userService)
+        {
+            UserService = userService;
+        }
+
+        [BindProperty]
+        public UserModel UserProfile { get; set; }
+
+
+        public void OnGet(string id)
+        {
+            UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            UserService.UpdateUser(UserProfile);
+
+            return RedirectToPage("../Index");
+        }
+
+    }
+}
