4d227af023ca3ef76bc3e3ade3d62190702ee033
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-04-30 14:52:25 -0700

    Added separate page for updating profile bio

---
+++ src/Pages/Product/ProfileBioUpdate.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Product
+{
+    public class ProfileBioUpdateModel : PageModel
+    {
+        public JsonFileUserService UserService { get; }
+
+
+        public ProfileBioUpdateModel(JsonFileUserService userService)
+        {
+            UserService = userService;
+        }
+
+        [BindProperty]
+        public UserModel UserProfile { get; set; }
+
+
+        public void OnGet(string id)
+        {
+            UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            UserService.UpdateUserBio(UserProfile);
+
+            return RedirectToPage("../Index");
+        }
+    }
+}
