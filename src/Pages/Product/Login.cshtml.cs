3780ec984c771182d318a509d3d75b2caea7cd38
james <x0930186031@gmail.com>
2022-05-03 10:53:31 -0700

    Created Login.cshtml in Product field

---
+++ src/Pages/Product/Login.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class LoginModel : PageModel
+    {
+        // Data middle tier
+        public JsonFileUserService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public LoginModel(JsonFileUserService productService)
+        {
+            ProductService = productService;
+        }
+        [BindProperty]
+        public bool checkAccount { get; set; }
+        
+        [BindProperty]
+        public UserModel User { get; set; }
+         
+        // The data to show
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            User = ProductService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+        }
+        // On post
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            };
+             // checked if account exist or not.
+             checkAccount = ProductService.GetUsers().Any(m => m.username.Equals(User.username) && m.password.Equals(User.password));
+            if (checkAccount == false)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return Page();
+            }
+            HttpContext.Session.SetString("Username", User.username);
+            return RedirectToPage("../Index");
+
+        }
+
+    }
+}
