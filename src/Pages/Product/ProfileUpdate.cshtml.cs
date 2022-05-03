8f859dd408cb3043820fef2b02b8a3b752002da6
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-02 23:38:58 -0700

    Added comments to ProfileUpdate.cshtml.cs

--- src/Pages/Product/ProfileUpdate.cshtml.cs
+++ src/Pages/Product/ProfileUpdate.cshtml.cs
@@ -7,22 +7,35 @@ namespace SquidMeet.WebSite.Pages.Product
 {
     public class ProfileUpdateModel : PageModel
     {
+        // Data middle tier 
         public JsonFileUserService UserService { get; }
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="userService"></param>
         public ProfileUpdateModel(JsonFileUserService userService)
         {
             UserService = userService;
         }
 
+        // The data to show, bind to it for the pos
         [BindProperty]
         public UserModel UserProfile { get; set; }
 
-
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
         }
 
+        /// <summary>
+        /// REST Post request
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
