1a6d9e1a3428ce8e8ba3fd5dc18a0bc627a3f564
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-02 23:39:14 -0700

    Added comments to ProfilePasswordUpdate.cshtml.cs

--- src/Pages/Product/ProfilePasswordUpdate.cshtml.cs
+++ src/Pages/Product/ProfilePasswordUpdate.cshtml.cs
@@ -8,22 +8,35 @@ namespace SquidMeet.WebSite.Pages.Product
 {
     public class ProfilePasswordUpdateModel : PageModel
     {
+        // Data middle tier 
         public JsonFileUserService UserService { get; }
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="userService"></param>
         public ProfilePasswordUpdateModel(JsonFileUserService userService)
         {
             UserService = userService;
         }
 
-
+        // The data to show, bind to it for the post
         [BindProperty]
         public UserModel UserProfile { get; set; }
 
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
         }
 
+        /// <summary>
+        /// REST Post request
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
