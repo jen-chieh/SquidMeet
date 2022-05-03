b843a440829826ea20cd8b1a1b136cfe9437ad53
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-02 23:40:41 -0700

    Added comments to ProfileBioUpdate.cshtml.cs

--- src/Pages/Product/ProfileBioUpdate.cshtml.cs
+++ src/Pages/Product/ProfileBioUpdate.cshtml.cs
@@ -8,32 +8,42 @@ namespace SquidMeet.WebSite.Pages.Product
 {
     public class ProfileBioUpdateModel : PageModel
     {
+        // Data middle tier 
         public JsonFileUserService UserService { get; }
 
-
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="userService"></param>
         public ProfileBioUpdateModel(JsonFileUserService userService)
         {
             UserService = userService;
         }
 
+        // The data to show, bind to it for the post
         [BindProperty]
         public UserModel UserProfile { get; set; }
 
-
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
         }
 
+        /// <summary>
+        /// REST Post request
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
             UserService.UpdateUserBio(UserProfile);
-
             return RedirectToPage("../Index");
         }
     }
