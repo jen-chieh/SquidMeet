19999afbd4c2642c4cf539fff7c98f3ed142665d
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-02 23:40:54 -0700

    Added comments to ProfileAgeUpdate.cshtml.cs

--- src/Pages/Product/ProfileAgeUpdate.cshtml.cs
+++ src/Pages/Product/ProfileAgeUpdate.cshtml.cs
@@ -6,21 +6,42 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SquidMeet.WebSite.Pages.Product
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ProfileAgeUpdateModel : PageModel
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userService"></param>
         public ProfileAgeUpdateModel(JsonFileUserService userService)
         {
             UserService = userService;
         }
+
+        // Data middle tier 
         public JsonFileUserService UserService { get; }
 
+
+        // The data to show, bind to it for the post
         [BindProperty]
         public UserModel UserProfile { get; set; }
 
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
         }
+
+
+        /// <summary>
+        /// REST Post request
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
