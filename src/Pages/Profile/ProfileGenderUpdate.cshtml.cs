a6f13b68966de42c18eb1256d74c57967c8d1b37
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-05 09:51:55 -0700

    Moved ProfileGenderUpdate.cshtml and ProfileGenderUpdate.cshtml.cs to Pages/Profile

---
+++ src/Pages/Profile/ProfileGenderUpdate.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Product
+{
+    public class ProfileGenderUpdateModel : PageModel
+    {
+        // Data middle tier 
+        public JsonFileUserService UserService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="userService"></param>
+        public ProfileGenderUpdateModel(JsonFileUserService userService)
+        {
+            UserService = userService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public UserModel UserProfile { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            UserProfile = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+
+        }
+
+        /// <summary>
+        /// REST Post request
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            UserService.UpdateUserGender(UserProfile);
+
+            return RedirectToPage("../Index");
+        }
+    }
+}
