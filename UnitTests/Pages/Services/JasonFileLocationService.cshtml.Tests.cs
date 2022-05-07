30458f4a924cbaff080e9ec80a3478ad7698c995
jen-chieh <x0930186031@gmail.com>
2022-05-06 19:00:56 -0700

    Added JasonFileLocationService.cshtml.Tests.cs with 100% code coverage.

---
+++ UnitTests/Pages/Services/JasonFileLocationService.cshtml.Tests.cs
@@ -0,0 +1,75 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Services.JsonFileLocationService
+{
+    public class LocationTests
+    {
+        #region TestSetup
+        public static LocationModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LocationModel()
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Location()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetAllData().Count();
+            // Act
+            //  pageModel.OnGet("0");
+
+            // Assert
+            Assert.AreEqual(data, TestHelper.ProductService.GetAllData().Count());
+           
+        }
+        #endregion OnGet
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Sort_by_Location()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.sortByLocation().Count();
+            // Act
+            //  pageModel.OnGet("0");
+
+            // Assert
+            Assert.AreEqual(data, TestHelper.ProductService.sortByLocation().Count());
+
+        }
+        #endregion OnGet
+
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Sort_by_Rating()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.sortByRate().Count();
+            // Act
+            //  pageModel.OnGet("0");
+
+            // Assert
+            Assert.AreEqual(data, TestHelper.ProductService.sortByRate().Count());
+
+        }
+        #endregion OnGet
+
+    }
+}
