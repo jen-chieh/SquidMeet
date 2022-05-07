1cf96ea8eaba43d6cf62418e0486c99301275725
jen-chieh <x0930186031@gmail.com>
2022-05-07 12:55:42 -0700

    Added 100% code coverage for unit tests on the Login.cshtml.Test

---
+++ UnitTests/Pages/Product/Login.cshtml.Tests.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Pages.Product.Login
+{
+    /// <summary>
+    /// Test for CreateAccount.cshtml and CreateUserModelModel with valid and invalid onPost calls
+    /// </summary>
+    public class CreateAccountTests
+    {
+        #region TestSetup
+        public static LoginModel pageModel;
+
+        // Create new CreateUserModel
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LoginModel(TestHelper.UserService)
+            {
+            };
+
+        }
+
+        #endregion TestSetup
+
+        // Create new user and update user count
+        #region OnPost
+      
+        [Test]
+        // Test to verify OnPost adding new account with invalid values results in invalid model state
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.User = new UserModel
+            {
+                user_id = "bogus",
+                email = "bogus",
+                password = "bogus",
+                name = "bogus",
+                age = 30,
+                gender = "bogus",
+                bio = "bogus"
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost(pageModel.User) as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPost
+    }
+}
