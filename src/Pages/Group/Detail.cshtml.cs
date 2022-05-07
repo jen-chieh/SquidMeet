2d415faede2d9482d5a12586d00fc664193d9d03
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-06 17:08:05 -0700

    Created Detail.csthml.cs to show meetup details

---
+++ src/Pages/Group/Detail.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Group
+{
+    public class DetailModel : PageModel
+    {
+        // Data middle tier service
+        public JsonFileMeetupService MeetupService { get; }
+
+
+        public DetailModel(JsonFileMeetupService meetupService)
+        {
+            MeetupService = meetupService;
+        }
+
+        public MeetupModel Meetup;
+        public void OnGet(string id)
+        {
+            Meetup = MeetupService.GetMeetups().FirstOrDefault(g => g.meetup_id == id);
+        }
+    }
+}
