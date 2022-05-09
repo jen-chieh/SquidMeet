747cd627d7dbf39f56b8c9d615543156ce9c4339
Tummala Kalyani <tkalyani@seattleu.edu>
2022-05-08 20:16:16 -0700

    Added 100% code coverage for JsonFileLocationTypeService.cshtml.cs

---
+++ UnitTests/Pages/Services/JsonFileLocationTypeService.cshtml.Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Services.JsonFileLocationTypeService
+{
+    class LocationTypeTests
+    {
+        #region TestSetup
+        public static LocationTypeModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LocationTypeModel()
+            {
+                type_id = "3", type_name="Library"
+            };
+        }
+
+        #endregion TestSetup
+
+        #region GetLocationType
+        // Test to verify OnGet returns correct Locationdata with  is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Location_Type()
+        {
+            // Arrange
+            var locationTypeJson = TestHelper.LocationTypeService.GetLocationTypes();
+            // Act
+            var data = locationTypeJson.ToList();
+
+            // Assert
+          
+            Assert.AreEqual(3, data.Count());
+            Assert.AreEqual("0", data[0].type_id);
+            Assert.AreEqual("Bars or Cafes", data[0].type_name);
+            Assert.AreEqual("1", data[1].type_id);
+            Assert.AreEqual("Parks", data[1].type_name);
+            Assert.AreEqual("2", data[2].type_id);
+            Assert.AreEqual("Conference Rooms", data[2].type_name);
+
+        }
+        #endregion GetLocationTypes
+    }
+}
