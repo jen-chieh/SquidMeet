3acb1633cea699a3b1ecad319df3ddaea1f8ec97
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-09 02:19:14 -0700

    Added page of AcceptInvite.cshtml.cs

---
+++ src/Pages/Group/AcceptInvite.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquidMeet.WebSite.Pages.Group
+{
+    public class AcceptInviteModel : PageModel
+    {
+        public JsonFileMeetupService MeetupService { get; }
+        public JsonFileUserService UserService { get; }
+
+        public AcceptInviteModel(JsonFileMeetupService meetupService, JsonFileUserService userService)
+        {
+            UserService = userService;
+
+            MeetupService = meetupService;
+        }
+
+        [BindProperty]
+        public MeetupModel Meetup { get; set; }
+
+        [BindProperty]
+        public UserModel User { get; set; }
+
+
+        public void OnGet(string id)
+        {
+            User = UserService.GetUsers().FirstOrDefault(m => m.user_id.Equals(id));
+        }
+
+        public IActionResult OnPost(MeetupModel Meetup)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            MeetupService.AddAttendee(Meetup, "Test_Azkaban");
+
+
+            return RedirectToPage("ViewMyGroup");
+        }
+    }
+}
