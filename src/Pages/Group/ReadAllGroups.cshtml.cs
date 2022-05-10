7f68a7a5369dc10fddf96bbad58b18be6ae0a1c4
jen-chieh <x0930186031@gmail.com>
2022-05-09 21:16:25 -0700

    Added  JsonFileMeetupService Construction to ReadAllGroup.cshtml

--- src/Pages/Group/ReadAllGroups.cshtml.cs
+++ src/Pages/Group/ReadAllGroups.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SquidMeet.WebSite.Pages.Group
 {
@@ -7,6 +10,21 @@ namespace SquidMeet.WebSite.Pages.Group
     /// </summary>
     public class ReadAllGroupModel : PageModel
     {
+        // Data middletier
+        public JsonFileMeetupService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public ReadAllGroupModel(JsonFileMeetupService productService)
+        {
+            ProductService = productService;
+        }
+
+      
+      
         // On get
         public void OnGet()
         {
