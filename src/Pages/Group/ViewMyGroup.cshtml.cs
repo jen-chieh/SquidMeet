7406a7c6790a0705989a61ea0fddf9ca804d2ed9
Selina Hu <Selinaehu@gmail.com>
2022-05-09 22:00:10 -0700

    Added comments to ViewMyGroup.cshtml.cs.

--- src/Pages/Group/ViewMyGroup.cshtml.cs
+++ src/Pages/Group/ViewMyGroup.cshtml.cs
@@ -7,11 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace SquidMeet.WebSite.Pages.Group
 {
     /// <summary>
-    /// This is the  model for the update group page.
+    /// This is the model for the update group page
     /// </summary>
     public class UpdateGroupModel : PageModel
     {
-        // 
+        // Data middle tier
         public JsonFileMeetupService MeetupService { get; }
 
         /// <summary>
@@ -28,17 +28,23 @@ namespace SquidMeet.WebSite.Pages.Group
         [BindProperty]
         public MeetupModel Group { get; set; }
 
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="HostId"></param>
         public void OnGet(string HostId)
         {
             Group = MeetupService.GetMeetups().First(g => g.Host == HostId);
         }
 
         /// <summary>
-        /// REST Get request
-        /// Loads the Data
+        /// Post the model back to the page
+        /// The model is in the class variable Group
+        /// Call the data layer to Update that data
+        /// Then return to the Index page
         /// </summary>
-        /// <param name="HostId"></param>
-        /// s>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
