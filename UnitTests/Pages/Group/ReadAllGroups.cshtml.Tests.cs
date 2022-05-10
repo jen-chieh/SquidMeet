1f21e98b35a3ae732c81f6d8c3b6fc169f52004a
jen-chieh <x0930186031@gmail.com>
2022-05-09 21:17:01 -0700

    Added 100% code coverage to ReadAllGroups.cshtml

---
+++ UnitTests/Pages/Group/ReadAllGroups.cshtml.Tests.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using SquidMeet.WebSite.Pages.Group;
+
+namespace UnitTests.Pages.Group.Update
+{
+    public class ReadAllGroupsTests
+    {
+        #region TestSetup
+        public static ReadAllGroupModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadAllGroupModel(TestHelper.MeetupService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            var data=  pageModel.ProductService.GetMeetups();
+            
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.IsNotNull(data);
+          
+        }
+        #endregion OnGet
+
+    }
+}
