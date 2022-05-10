9daed10d5917a143a2739a6deadd9598c35ccd0f
Selina Hu <Selinaehu@gmail.com>
2022-05-09 21:54:30 -0700

    Added comments to Detail.cshtml.cs.

--- src/Pages/Group/Detail.cshtml.cs
+++ src/Pages/Group/Detail.cshtml.cs
@@ -5,18 +5,30 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SquidMeet.WebSite.Pages.Group
 {
+    /// <summary>
+    /// Model for showing meetup details 
+    /// </summary>
     public class DetailModel : PageModel
     {
         // Data middle tier service
         public JsonFileMeetupService MeetupService { get; }
 
-
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="meetupService"></param>
         public DetailModel(JsonFileMeetupService meetupService)
         {
             MeetupService = meetupService;
         }
 
+        // The data to show
         public MeetupModel Meetup;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             Meetup = MeetupService.GetMeetups().FirstOrDefault(g => g.meetup_id == id);
