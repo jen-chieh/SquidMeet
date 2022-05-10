683ce1be9dd1712948dcde722b534c1789e9d729
Tummala Kalyani <tkalyani@seattleu.edu>
2022-05-09 21:36:40 -0700

    Added 100% code coverage for Group.ViewMyGroup.cshtml.cs

---
+++ UnitTests/Pages/Group/ViewMyGroup.cshtml.Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using NUnit.Framework;
+
+using SquidMeet.WebSite.Pages.Group;
+
+namespace UnitTests.Pages.Group.ViewMyGroupTests
+{
+    class ViewMyGroupTests
+    {
+        #region TestSetup
+        // Initialize IndexModel
+
+        public static UpdateGroupModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new UpdateGroupModel(TestHelper.MeetupService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        // Test to verify the model state is valid with products
+        [Test]
+        public void OnGet_Valid_Should_Return_Groups()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("Will");
+
+
+
+            // Assert
+            Assert.AreEqual("Feet First Walks", pageModel.Group.Title);
+        }
+        #endregion OnGet
+
+        #region OnPost
+        // Test to verify the model state is valid with products
+        [Test]
+        public void OnPost_Valid_Should_Return_Groups()
+        {
+            // Arrange
+
+            // Act
+            pageModel.Group = new MeetupModel
+            {
+                Title = "New title"
+            };
+
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual("../Index", result.PageName);
+        }
+        #endregion OnGet
+    }
+}
