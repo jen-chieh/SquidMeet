d57748e98133912cbbcabfe5efb5273a19aba6dc
jen-chieh <x0930186031@gmail.com>
2022-05-09 20:51:04 -0700

    Added 100% code coverage to Update,cshtml in Meetup update

---
+++ UnitTests/Pages/Group/Update.cshtml.Tests.cs
@@ -0,0 +1,103 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using SquidMeet.WebSite.Pages.Group;
+
+namespace UnitTests.Pages.Group.Update
+{
+    public class MeetUpdateTests
+    {
+        #region TestSetup
+        public static MeetupUpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new MeetupUpdateModel(TestHelper.MeetupService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("59170836-95ac-42a5-833f-9f026c8dc152");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Feet First Walks", pageModel.Product.Title);
+        }
+        #endregion OnGet
+
+        #region OnPostAsync
+        // Test to verify OnPost update meetup with valid values keeps valid model state
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+            pageModel.Product = new MeetupModel
+            {
+                meetup_id= "ab012001-2db8-4270-8fcb-c2f870dae5f1test",
+                location = "Seattle",
+                LocationType= "Indoor",
+                Title = "Intro to Javascript",
+                 Date = "11/22/12",
+                Attendees= null,
+                Host  = "YJ",
+                Description = "JavaScript is among the most powerful and flexible programming languages of the web. It powers the dynamic behavior on most websites. It is one of the most widely used programming languages (Front-end as well as Back-end) and it has its presence in almost every area of software development. This Workshop is specifically designed to build your JavaScript skills from scratch, we will cover JavaScript\u0027s key fundamental features.",
+                InviteCode = "52553b96-7519-4988-aa0e-999afb46f44c",
+                Img = "https://raw.githubusercontent.com/youjin6/picbed2/main/code/img/store/ok20220506135934.jpeg",
+                Video= "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/33372b34-4d33-404b-a296-59691e1af211/pexels-mikhail"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("ViewMyGroup"));
+        }
+
+        [Test]
+        // Test to verify OnPost update meetup with invalid values results in invalid model state
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.Product = new MeetupModel
+            {
+                meetup_id = "ab012001-2db8-4270-8fcb-c2f870dae5f1test",
+                location = "Seattle",
+                LocationType = "Indoor",
+                Title = "Intro to Javascript",
+                Date = "11/22/12",
+                Attendees = null,
+                Host = "YJ",
+                Description = "JavaScript is among the most powerful and flexible programming languages of the web. It powers the dynamic behavior on most websites. It is one of the most widely used programming languages (Front-end as well as Back-end) and it has its presence in almost every area of software development. This Workshop is specifically designed to build your JavaScript skills from scratch, we will cover JavaScript\u0027s key fundamental features.",
+                InviteCode = "52553b96-7519-4988-aa0e-999afb46f44c",
+                Img = "https://raw.githubusercontent.com/youjin6/picbed2/main/code/img/store/ok20220506135934.jpeg",
+                Video =""
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPostAsync
+    }
+}
