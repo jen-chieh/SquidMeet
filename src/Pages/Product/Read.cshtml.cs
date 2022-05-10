7b1e98b3155cc866773b0c78517c8921168902d4
Selina Hu <Selinaehu@gmail.com>
2022-05-09 22:05:09 -0700

    Removed old code from Read.cshtml.cs.

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -7,6 +7,9 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Model to read location information from the database
+    /// </summary>
     public class ReadModel : PageModel
     {
         // Data middle tier
@@ -33,31 +36,5 @@ namespace ContosoCrafts.WebSite.Pages.Product
         {
             Product  = ProductService.GetAllData().FirstOrDefault(m => m.location_id.Equals(id));
         }
-
-        /*
-        // Data middle tier for location hours
-        public JsonFileLocationHoursService LocationHoursService { get; }
-
-        /// <summary>
-        /// Defualt Construtor for location hours as parameter
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="locationHoursService"></param>
-        public ReadModel(JsonFileLocationHoursService locationHoursService)
-        {
-            LocationHoursService = locationHoursService;
-        }
-
-        // The data to show for location hours
-        public LocationHoursModel LocationHours;
-
-        /// <summary>
-        /// REST Get request for location hours
-        /// </summary>
-        /// <param name="id"></param>
-        public void OnGetLocationHours(string id)
-        {
-            LocationHours = LocationHoursService.GetLocationHours().FirstOrDefault(m => m.location_hours_id.Equals(id));
-        }*/
     }
 }
