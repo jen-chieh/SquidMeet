b6399c7d6ab099609ef57361691f888037d77f6d
Selina Hu <Selinaehu@gmail.com>
2022-05-09 23:14:52 -0700

    Added 100% code coverage to Detail.cshtml.Tests.cs.

---
+++ UnitTests/Pages/Group/Detail.cshtml.Tests.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using SquidMeet.WebSite.Pages.Group;
+
+namespace UnitTests.Pages.Group.Update
+{
+    /// <summary>
+    /// Unit Tests for onget method for Detail model
+    /// </summary>
+    public class DetailModelTests
+    {
+        #region TestSetup
+        // Data middle tier
+        public static DetailModel pageModel;
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="meetupService"></param>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new DetailModel(TestHelper.MeetupService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        // Test to verify OnGet returns correct data with a given id and model state is valid
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("ebf80f1c-3802-4df9-bbda-fd274810a44c");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Meditate with a Monk", pageModel.Meetup.Title);
+        }
+        #endregion OnGet
+
+    }
+}
