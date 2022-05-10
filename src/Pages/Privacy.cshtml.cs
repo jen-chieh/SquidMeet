3a7614c8a91b8b57d4ab8a8c3e5352db65886954
Selina Hu <Selinaehu@gmail.com>
2022-05-09 22:18:36 -0700

    Added comments to Privacy.cshtml.cs.

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,12 +3,23 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for privacy page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Data middle tier
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
 
+        /// <summary>
+        /// REST Get request
+        /// </summary>
         public void OnGet()
         {
         }
