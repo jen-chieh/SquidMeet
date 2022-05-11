4893ba55233a84076fc02505f89fc8eabfb252ca
Selina Hu <Selinaehu@gmail.com>
2022-05-10 18:59:32 -0700

    Added comments to LocationhoursModelTest.cs.

--- UnitTests/Models/LocationhoursModelTest.cs
+++ UnitTests/Models/LocationhoursModelTest.cs
@@ -5,48 +5,38 @@ using Moq;
 using ContosoCrafts.WebSite.Models;
 
 
-
-
 namespace UnitTests.Models
 {
+    /// <summary>
+    /// Unit Test to check for expected values in AttendeeModel
+    /// </summary>
     [TestFixture]
     internal class LocationhoursModelTests
     {
+        // Data middle tier
         private LocationHoursModel _locationhoursModel;
 
-
-
-
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
         [SetUp]
         public void SetUp()
         {
             _locationhoursModel = new LocationHoursModel();
         }
 
-
-
-
+        // Test to verify model information is accurate
         [Test]
         public void GetLocationId_Valid_Test_Reading_Should_Return_ExpectedValue()
         {
             // Arrange
 
-
-
-
             // Act
             var expectedValue = "0";
-
-
-
 
             // Assert
             _locationhoursModel.location_hours_id = expectedValue;
             Assert.AreEqual(expectedValue, _locationhoursModel.location_hours_id);
-
-
-
-
         }
     }
 }
