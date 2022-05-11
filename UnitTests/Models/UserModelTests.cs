1c3bdbd0f44aa680da5d32368d42ee4d80d90e4d
Selina Hu <Selinaehu@gmail.com>
2022-05-10 19:04:01 -0700

    Added comments to UserModelTests.cs.

--- UnitTests/Models/UserModelTests.cs
+++ UnitTests/Models/UserModelTests.cs
@@ -5,18 +5,26 @@ using Moq;
 using ContosoCrafts.WebSite.Models;
 
 namespace UnitTests.Models
-{   
+{
+    /// <summary>
+    /// Unit Test to check for expected values in UserModel
+    /// </summary>
     [TestFixture]
     internal class UserModelTests
     {
+        // Data middle tier
         private UserModel _user;
 
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
         [SetUp]
         public void SetUp()
         {
             _user = new UserModel();
         }
 
+        // Test to verify model information is accurate
         [Test]
         public void GetUserId_Valid_Test_Reading_Should_Return_ExpectedValue()
         {
