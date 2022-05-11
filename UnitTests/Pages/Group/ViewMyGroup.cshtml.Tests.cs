9b5ad4565f62b183beda2d594b307b1cf19ba054
Selina Hu <Selinaehu@gmail.com>
2022-05-10 19:10:16 -0700

    Added comments to ViewMyGroup.cshtml.Tests.cs.

--- UnitTests/Pages/Group/ViewMyGroup.cshtml.Tests.cs
+++ UnitTests/Pages/Group/ViewMyGroup.cshtml.Tests.cs
@@ -10,13 +10,18 @@ using SquidMeet.WebSite.Pages.Group;
 
 namespace UnitTests.Pages.Group.ViewMyGroupTests
 {
+    /// <summary>
+    /// Unit Tests for onget and onpost methods for UpdateGroup model
+    /// </summary>
     class ViewMyGroupTests
     {
         #region TestSetup
-        // Initialize IndexModel
+        // Initialize UpdateGroupModel (data middle tier)
 
         public static UpdateGroupModel pageModel;
-
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
