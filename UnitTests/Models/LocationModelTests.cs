6c63e0263ec694bec7372b6f5b1e4b494d186361
Selina Hu <Selinaehu@gmail.com>
2022-05-10 19:01:21 -0700

    Added comments to LocationModelTests.cs.

--- UnitTests/Models/LocationModelTests.cs
+++ UnitTests/Models/LocationModelTests.cs
@@ -5,18 +5,26 @@ using Moq;
 using ContosoCrafts.WebSite.Models;
 
 namespace UnitTests.Models
-{   
+{
+    /// <summary>
+    /// Unit Test to check for expected values in LocationModel
+    /// </summary>
     [TestFixture]
     internal class LocationModelTests
     {
+        // Data middle tier
         private LocationModel _locationModel;
 
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
         [SetUp]
         public void SetUp()
         {
             _locationModel = new LocationModel();
         }
 
+        // Test to verify model information is accurate
         [Test]
         public void GetLocationId_Valid_Test_Reading_Should_Return_ExpectedValue()
         {
