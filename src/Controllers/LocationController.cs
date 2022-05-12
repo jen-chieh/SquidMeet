8fcbda6e8ab2eca1edfa22c97e09b57768502d7b
Selina Hu <Selinaehu@gmail.com>
2022-05-12 16:05:14 -0700

    Updated comments to follow coding guidelines in LocationController.cs.

--- src/Controllers/LocationController.cs
+++ src/Controllers/LocationController.cs
@@ -7,23 +7,40 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+
+    /// <summary>
+    /// Create class from location.json file and retrieve all data from file.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class LocationController : ControllerBase
     {
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        /// <param name="productService"></param>
         public LocationController(JsonFileLocationService productService)
         {
             ProductService = productService;
         }
+
         // Data middle tier
         public JsonFileLocationService ProductService { get; }
-        // Get user information from model
+
+        /// <summary>
+        /// Get database information from model
+        /// </summary>
         [HttpGet]
         public IEnumerable<LocationModel> Get()
         {
             return ProductService.GetAllData();
         }
 
+        /// <summary>
+        /// Request to add rating and return status result
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,10 +49,18 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// RatingRequest class contains a productId the rating is for
+        /// and the star rating represented as an int
+        /// </summary>
         public class RatingRequest
         {
+            // ID of product to be rated
             public string ProductId { get; set; }
+
+            // int representation of star rating
             public int Rating { get; set; }
+
         }
     }
 }
