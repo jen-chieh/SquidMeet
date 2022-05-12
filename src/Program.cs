0b87aa12a0d653ab0ebc0148252c5226f7099fb0
Selina Hu <Selinaehu@gmail.com>
2022-05-12 16:12:59 -0700

    Updated comments to follow coding guidelines in Program.cs.

--- src/Program.cs
+++ src/Program.cs
@@ -3,17 +3,30 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// Program called by runtime to add services and configure HTTP requests
+    /// </summary>
     public class Program
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
         public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        // This method gets called by the runtime. Use this method to configure the HTTP request's pipeline. 
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to 
+        /// configure the HTTP request's pipeline. 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
