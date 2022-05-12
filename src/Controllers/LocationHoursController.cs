18ac3c1594fddb7c4930046207b2e6f4000aebc7
Selina Hu <Selinaehu@gmail.com>
2022-05-12 16:05:29 -0700

    Updated comments to follow coding guidelines in LocationHoursController.cs.

--- src/Controllers/LocationHoursController.cs
+++ src/Controllers/LocationHoursController.cs
@@ -5,20 +5,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+
+    /// <summary>
+    /// Create class from locationhours.json file and retrieve all data from file.
+    /// </summary>
+
     [ApiController]
     [Route("[controller]")]
     public class LocationHoursController : ControllerBase
     {
-         // Initialize user controller
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        /// <param name="locationHoursService"></param>
         public LocationHoursController(JsonFileLocationHoursService locationHoursService) => 
             LocationHoursService = locationHoursService;
 
         // Data middle tier
         public JsonFileLocationHoursService LocationHoursService { get; }
 
-        // Get location hours from model
+        /// <summary>
+        /// Get database information from model
+        /// </summary>
         [HttpGet]
         public IEnumerable<LocationHoursModel> Get() => LocationHoursService.GetLocationHours();
-
     }
 }
