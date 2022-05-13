e9919bc0e52912dc897a7d11c18fd5cf7d50f86b
Selina Hu <Selinaehu@gmail.com>
2022-05-12 17:26:34 -0700

    Updated comments and code formatting in JsonFileLocationService.cshtml.Tests.cs.

--- UnitTests/Pages/Services/JsonFileLocationService.cshtml.Tests.cs
+++ UnitTests/Pages/Services/JsonFileLocationService.cshtml.Tests.cs
@@ -11,13 +11,13 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
 {
     /// <summary>
     /// Unit Tests for onget, getlocations, getsortbylocationtypes,
-    /// getsortbyratinglocations, addfirstrating, and addrating methods for location model
+    /// getsortbyratinglocations, addfirstrating, and addrating methods for location service
     /// </summary>
     public class LocationTests
     {
         #region TestSetup
-        // Data middle tier
 
+        // Data middle tier
         public static LocationModel pageModel;
 
         /// <summary>
@@ -34,7 +34,10 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
         #endregion TestSetup
 
         #region GetLocatoins
-        // Test to verify OnGet returns correct Locationdata with  is valid
+
+        /// <summary>
+        /// Test to verify OnGet returns correct data with a given id and model state is valid
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Location()
         {
@@ -48,32 +51,39 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
 
         }
         #endregion GetLocations
+
         #region GetSortByLocationTypes
-        // Test to verify OnGet returns correct data with a given id and model state is valid
+        /// <summary>
+        /// Test to verify OnGet returns correct data with a given id and model state is valid
+        /// for sorting by location types
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Sort_by_Location_Types()
         {
             // Arrange
             var data = TestHelper.ProductService.sortByLocation().ToList();
+
             // Act
-            //  pageModel.OnGet("0");
 
             // Assert
             Assert.IsNotNull(data);
-
         }
         #endregion GetSortByLocationTypes
 
         #region GetSortByRatingLocations
-        // Test to verify OnGet returns correct sorting data by rating is valid
+
+        /// <summary>
+        /// Test to verify OnGet returns correct data with a given id and model state is valid
+        /// for sorting by rating
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Sort_by_Rating()
         {
             // Arrange
             var data = TestHelper.ProductService.sortByRate(5).ToList();
             var data2 = TestHelper.ProductService.sortByRate(0).ToList();
+
             // Act
-            //  pageModel.OnGet("0");
 
             // Assert
             Assert.IsNotNull(data);
@@ -81,8 +91,13 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
 
         }
         #endregion GetSortByRatingLocations
-        // Test to verify location addrating is the first time to rate
+
+
         #region AddFirstRating
+
+        /// <summary>
+        /// Test to verify AddRating correctly adds a rating to a location with a null rating
+        /// </summary>
         [Test]
         public void AddRating_location_is_null_rating()
         {
@@ -95,8 +110,13 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
             Assert.IsNotNull(TestHelper.ProductService.GetAllData().First(x => x.location_id.Equals("0")));
         }
         #endregion  AddFirstRating
+
         #region AddRating
-        // Test to verify location has been rated and added new rating.
+
+        /// <summary>
+        /// Test to verify AddRating correctly updates the rating 
+        /// for a location with an existing rating
+        /// </summary>
         [Test]
         public void AddRating_location_is_rated()
         {
@@ -108,8 +128,7 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
             // Assert
             Assert.IsNotNull(TestHelper.ProductService.GetAllData().First(x => x.location_id.Equals("18")));
         }
+
         #endregion AddRating
     }
-
-
 }
