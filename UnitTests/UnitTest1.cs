97b6b5d1498b174d875f3095b910831ad6bac7c9
Selina Hu <Selinaehu@gmail.com>
2022-05-12 17:29:40 -0700

    Updated comments and code formatting in UnitTest1.cs.

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,19 +1,28 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace UnitTests
 {
+    /// <summary>
+    /// Basic unit test framework to use as a skeleton for other unit tests
+    /// </summary>
     public class Tests
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         [SetUp]
         public void Setup()
         {
         }
 
+        /// <summary>
+        /// Unit test outline to assert
+        /// </summary>
         [Test]
         public void Test1()
         {
-            // Putting a comment in.
             Assert.Pass();
         }
+
     }
 }
