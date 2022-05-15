da9e2c3a86b492f7cf3b119dbccfbeaffe6f22ef
tkalyani1997 <tkalyani@seattleu.edu>
2022-05-15 13:36:06 -0700

    Added 100% code coverage for LocationService in JsonFileLocationService.cshtml.Tests.cs

--- UnitTests/Pages/Services/JsonFileLocationService.cshtml.Tests.cs
+++ UnitTests/Pages/Services/JsonFileLocationService.cshtml.Tests.cs
@@ -130,5 +130,76 @@ namespace UnitTests.Pages.Services.JsonFileLocationService
         }
 
         #endregion AddRating
+
+        #region GetSortByLocationTypeAndRating
+
+        /// <summary>
+        /// Test to verify Get By Location type and rating 
+        /// </summary>
+        [Test]
+        public void GetLocationSortBy_LocationType_And_Rating_Test()
+        {
+            // Arrange
+            string bars_or_cafes_location_type = "0";
+            int rating = 5;
+            // Act
+            var dataset = TestHelper.ProductService.sortByLocationAndRating(bars_or_cafes_location_type, rating);
+            var test_result = dataset.ToList();
+            // Assert
+            Assert.AreEqual(1, test_result.Count);
+            Assert.AreEqual("0", test_result[0].type_id);
+            Assert.AreEqual("Harry\u0027s Bar", test_result[0].name);
+            Assert.AreEqual(5, test_result[0].rating.Sum() / test_result[0].rating.Length);
+        }
+
+        [Test]
+        public void GetLocationSortBy_LocationTypeNull_And_Rating_Test()
+        {
+            // Arrange
+            string location_type_null = null;
+            int rating = 5;
+            // Act
+            var dataset = TestHelper.ProductService.sortByLocationAndRating(location_type_null, rating);
+            var result = dataset.ToList();
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("0", result[0].type_id);
+            Assert.AreEqual("Harry\u0027s Bar", result[0].name);
+            Assert.AreEqual("2", result[1].type_id);
+            Assert.AreEqual(5, result[0].rating.Sum() / result[0].rating.Length);
+            Assert.AreEqual(5, result[1].rating.Sum() / result[1].rating.Length);
+        }
+
+        [Test]
+        public void GetLocationSortBy_LocationType_And_RatingZero_Test()
+        {
+            // Arrange
+            string bars_or_cafes_location_type = "0";
+            int rating = 0;
+            // Act
+            var dataset = TestHelper.ProductService.sortByLocationAndRating(bars_or_cafes_location_type, rating);
+            var result = dataset.ToList();
+            // Assert
+            Assert.AreEqual(6, result.Count);
+            Assert.AreEqual("0", result[0].type_id);
+            Assert.AreEqual("0", result[5].type_id);
+            Assert.AreEqual("Mr. West Cafe Bar - Downtown", result[0].name);
+          
+        }
+
+        [Test]
+        public void GetLocationSortBy_LocationTypeUnKnown_And_RatingUnknow_Test()
+        {
+            // Arrange
+            string location_type_Unknown = "10";
+            int rating_unknown = 100;
+            // Act
+            var dataset = TestHelper.ProductService.sortByLocationAndRating(location_type_Unknown, rating_unknown);
+            var result = dataset.ToList();
+            // Assert
+            Assert.AreEqual(0, result.Count);
+
+        }
+        #endregion GetSortByLocationTypeAndRating
     }
 }
