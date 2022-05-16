bbba773cea318c782a52b316f3e4ddb1ab961eb4
tkalyani1997 <tkalyani@seattleu.edu>
2022-05-15 21:35:44 -0700

    Added 100% code coverage in LocationTypeTest.cs

---
+++ UnitTests/Controllers/LocationTypeControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Controllers;
+using NUnit.Framework;
+
+namespace UnitTests.Controllers
+{
+    class LocationTypeControllerTest
+    {
+        #region TestSetup
+
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+
+        }
+
+        #endregion TestSetup
+
+        #region GetLocationTypeController
+
+        /// <summary>
+        /// Test to verify Controller Get to fetch all Location Types
+        /// </summary>
+        [Test]
+        public void OnGet_Controller_Should_Return_All_Location_Types()
+        {
+            // Arrange
+            var controller = new LocationTypeController(TestHelper.LocationTypeService);
+            // Act
+            var result = controller.Get().ToList();
+
+            // Assert
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("Parks", result[1].type_name);
+
+        }
+        #endregion GetLocationTypeController
+    }
+}
