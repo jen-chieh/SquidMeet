466791b754cf0b9f2ad679656b45540ca1ecc190
tkalyani1997 <tkalyani@seattleu.edu>
2022-05-15 21:38:01 -0700

    Added 100% code coverage in UserControllerTest.cs

---
+++ UnitTests/Controllers/UserControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Controllers;
+using NUnit.Framework;
+
+namespace UnitTests.Controllers
+{
+    class UserControllerTest
+    {
+        #region TestSetup
+
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+
+        }
+
+        #endregion TestSetup
+
+        #region GetUserController
+
+        /// <summary>
+        /// Test to verify Controller Get returns all Users
+        /// </summary>
+        [Test]
+        public void OnGet_Controller_Should_Return_All_Users()
+        {
+            // Arrange
+            var controller = new UserController(TestHelper.UserService);
+            // Act
+            var result = controller.Get().ToList();
+
+            // Assert
+            Assert.AreEqual(12, result.Count());
+            Assert.AreEqual("Blanchard Whitehead", result[0].name);
+
+        }
+        #endregion GetUserController
+    }
+}
