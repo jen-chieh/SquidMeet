60b102337b87834450722998a6c81dcd6ee90d3a
tkalyani1997 <tkalyani@seattleu.edu>
2022-05-15 21:33:34 -0700

    Added 100% code coverage in LocationControllerTest.cs

---
+++ UnitTests/Controllers/LocationControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Controllers;
+using NUnit.Framework;
+using static ContosoCrafts.WebSite.Controllers.LocationController;
+
+namespace UnitTests.Controllers
+{
+    class LocationControllerTest
+    {
+        #region TestSetup
+
+        /// <summary>
+        /// Default Construtor
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            
+        }
+
+        #endregion TestSetup
+
+        #region GetLocationController
+
+        /// <summary>
+        /// Test to Controller Get to return all Locations
+        /// </summary>
+        [Test]
+        public void OnGet_Controller_Should_Return_All_Location()
+        {
+            // Arrange
+            var controller = new LocationController(TestHelper.ProductService);
+            // Act
+           var result = controller.Get().ToList();
+
+            // Assert
+            Assert.AreEqual(20, result.Count());
+
+        }
+        #endregion GetLocationController
+
+        #region PatchLocationController
+
+        /// <summary>
+        /// Test to Controller on Patch to update Rating of a location
+        /// </summary>
+        [Test]
+        public void OnPatch_Controller_Should_Return_Ok()
+        {
+            // Arrange
+            var controller = new LocationController(TestHelper.ProductService);
+            RatingRequest ratingRequest = new RatingRequest()
+            {
+                ProductId = "0",
+                Rating = 1
+            };
+            // Act
+            var update = controller.Patch(ratingRequest);
+
+            var final_result = controller.Get().ToList();
+
+
+            // Assert
+            Assert.AreEqual(1, final_result[0].rating[0]);
+
+        }
+        #endregion PatchLocationController
+    }
+}
