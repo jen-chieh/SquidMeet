38cd684002e90d8b069bec93a697fd830c3a8cf5
Selina Hu <Selinaehu@gmail.com>
2022-05-16 18:24:24 -0700

    Removed unused "using" statements in LocationTypeController.cs.

--- src/Controllers/LocationTypeController.cs
+++ src/Controllers/LocationTypeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
