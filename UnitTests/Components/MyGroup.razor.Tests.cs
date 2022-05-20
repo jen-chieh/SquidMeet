bad88bd44abc4c12e9471900a8c47d46e5e7eed4
Selina Hu <Selinaehu@gmail.com>
2022-05-19 23:14:53 -0700

    Added 100% code coverage for MyGroup.razor.Tests.cs.

---
+++ UnitTests/Components/MyGroup.razor.Tests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using SquidMeet.WebSite.Components;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Components
+{
+    /// <summary>
+    /// Unit tests for MyGroup.razor utilizing Bunit for testing that the page markups
+    /// include expected event titles
+    /// </summary>
+    public class MyGroupTests : BunitTestContext
+    {
+        #region TestSetup
+
+        [SetUp]
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        [Test]
+        /// <summary>
+        /// Test to ensure the my group page markup contains the title of one of the default events
+        /// </summary>
+        public void MyGroup_Default_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileMeetupService>(TestHelper.MeetupService);
+
+            // Act
+            var page = RenderComponent<MyGroup>();
+
+            // Get the Cards retrned
+            var result = page.Markup;
+
+            // Assert
+            Assert.AreEqual(true, result.Contains("Intro to Javascript"));
+        }
+
+    }
+}
