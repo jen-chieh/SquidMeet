6f3e97dd72f5c093331e2d4b52fef44c6db84708
james <x0930186031@gmail.com>
2022-05-20 19:28:45 -0700

    Added crerateData feature to JsonFileLocationHoursSerice

--- src/Services/JsonFileLocationHoursService.cs
+++ src/Services/JsonFileLocationHoursService.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using ContosoCrafts.WebSite.Models;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +13,10 @@ namespace ContosoCrafts.WebSite.Services
     /// </summary>
     public class JsonFileLocationHoursService
     {
+        public JsonFileLocationHoursService()
+        {
+        }
+
         /// <summary>
         /// Default Constructor
         /// </summary>
@@ -40,6 +46,55 @@ namespace ContosoCrafts.WebSite.Services
                 });
 
         }
+
+        /// <summary>
+        /// Save All products data to storage
+        /// </summary>
+        /// <param name="products"></param>
+        public void SaveData(IEnumerable<LocationHoursModel> locationhours)
+        {
+
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<LocationHoursModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    locationhours
+                );
+            }
+        }
+
+        /// <summary>
+        /// Create a new locationHours using default values
+        /// After create the user can update to set values
+        /// </summary>
+        /// <returns></returns>
+        public LocationHoursModel CreateData( String id,LocationHoursModel locationHours)
+        {
+           
+            var datahours = new LocationHoursModel()
+            {
+                location_hours_id = id,
+                location_mon_hours = locationHours.location_mon_hours,
+                location_tues_hours = locationHours.location_tues_hours,
+                location_wed_hours = locationHours.location_wed_hours,
+                location_thurs_hours = locationHours.location_thurs_hours,
+                location_fri_hours = locationHours.location_fri_hours,
+                location_sat_hours = locationHours.location_sat_hours,
+                location_sun_hours = locationHours.location_sun_hours,
+
+            };
+
+            // get the currrent locationhours set and append the new record to it
+            var hoursDataset = GetLocationHours();
+            hoursDataset = hoursDataset.Append(datahours);
+            SaveData(hoursDataset);
+            return datahours;
+        }
+
 
     }
 }
