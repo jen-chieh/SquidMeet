d3a3491bae334894a09b3b0688bdf04aee6fbfaf
james <x0930186031@gmail.com>
2022-05-20 19:27:43 -0700

    Added defensive feature when User creates location to create.cshtml

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -12,29 +12,55 @@ namespace ContosoCrafts.WebSite.Pages.Product
     {
         // Data middle tier
         public JsonFileLocationService ProductService { get; }
+        public JsonFileLocationHoursService LocationHoursService { get; }
 
         /// <summary>
         /// Defualt Construtor
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="productService"></param>
-        public CreateModel(JsonFileLocationService productService)
+        public CreateModel(JsonFileLocationService productService, JsonFileLocationHoursService locationHoursservice)
         {
             ProductService = productService;
+            LocationHoursService = locationHoursservice;
         }
 
         // The data to show
         public LocationModel Product;
 
+
+        public LocationHoursModel locationHoursModel;
+
         /// <summary>
         /// REST Get request
         /// </summary>
+        ///
+        /*
         public IActionResult OnGet()
         {
-            Product  = ProductService.CreateData();
+           // Product  = ProductService.CreateData();
 
-            return RedirectToPage("./Update", new { Id = Product.location_id });
+           return RedirectToPage("./Update", new { Id = Product.location_id });
+           
         }
+
+        */
+
+        public IActionResult OnPost(LocationModel Product, LocationHoursModel locationHoursModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+           string id =  ProductService.CreateData(Product).location_id;
+
+            LocationHoursService.CreateData(id, locationHoursModel);
+
+            return RedirectToPage("../Index");
+        }
+
+
 
     }
 }
