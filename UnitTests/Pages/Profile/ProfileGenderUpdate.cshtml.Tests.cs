839d6c6ec8f137a9d1053df8ac0c82e29351b18f
Selina Hu <Selinaehu@gmail.com>
2022-05-23 19:25:22 -0700

    Removed unused "using" statements in ProfileGenderUpdate.cshtml.Tests.cs.

--- UnitTests/Pages/Profile/ProfileGenderUpdate.cshtml.Tests.cs
+++ UnitTests/Pages/Profile/ProfileGenderUpdate.cshtml.Tests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ContosoCrafts.WebSite.Models;
+﻿using ContosoCrafts.WebSite.Models;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using SquidMeet.WebSite.Pages.Product;
