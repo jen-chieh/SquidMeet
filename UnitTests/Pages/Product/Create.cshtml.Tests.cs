8e4d4c7f1f8664dabd3be5c4393c65dfb40324e6
Selina Hu <Selinaehu@gmail.com>
2022-05-23 18:00:42 -0700

    Added 100% code coverage for Create.cshtml.Tests.cs.

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages.Product;
 using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Product.Create
 {
@@ -19,8 +20,11 @@ namespace UnitTests.Pages.Product.Create
         // Data middle tier
         public static LocationModel pageLocationModel;
 
+        // Data middle tier
+        public static LocationHoursModel pageLocationHoursModel;
+
         /// <summary>
-        /// Defualt Construtor
+        /// Default Constructor
         /// </summary>
         [SetUp]
         public void TestInitialize()
@@ -30,6 +34,10 @@ namespace UnitTests.Pages.Product.Create
             };
 
             pageLocationModel = new LocationModel()
+            {
+            };
+
+            pageLocationHoursModel = new LocationHoursModel()
             {
             };
         }
@@ -55,6 +63,59 @@ namespace UnitTests.Pages.Product.Create
             Assert.AreEqual(oldCount+1, TestHelper.ProductService.GetAllData().Count());
         }
         #endregion OnGet
+
+        #region OnPostAsync
+
+        /// <summary>
+        /// Test to verify OnPost update meetup with valid values keeps valid model state
+        /// </summary>
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // First Create the product
+            pageModel.Product = TestHelper.ProductService.CreateData(pageLocationModel);
+            pageModel.Product.name = "Example to Create";
+            TestHelper.ProductService.UpdateData(pageModel.Product);
+            pageModel.locationHoursModel = TestHelper.LocationHoursService.CreateData(pageModel.Product.location_id, pageLocationHoursModel);
+
+            // Act
+            var result = pageModel.OnPost(pageModel.Product, pageModel.locationHoursModel) as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        [Test]
+
+        /// <summary>
+        /// Test to verify OnPost update meetup with invalid values keeps invalid model state
+        /// </summary>
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange (invalid values)
+            pageModel.Product = new LocationModel
+            {
+                location_id = "bogus",
+                name = "bogus",
+                address = "bogus",
+                type_id = "bogus",
+                img = "bougus"
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost(pageModel.Product, pageModel.locationHoursModel) as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnPostAsync
 
     }
 }
