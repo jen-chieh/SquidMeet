a51013a629db08a98889535edd57e1b80129b72a
Selina Hu <Selinaehu@gmail.com>
2022-05-23 19:26:50 -0700

    Removed unused "using" statements in JsonFileLocationTypeService.cshtml.Tests.cs.

--- UnitTests/Services/JsonFileLocationTypeService.cshtml.Tests.cs
+++ UnitTests/Services/JsonFileLocationTypeService.cshtml.Tests.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Linq;
+﻿using System.Linq;
 using NUnit.Framework;
-using ContosoCrafts.WebSite.Pages.Product;
 using ContosoCrafts.WebSite.Models;
 
 namespace UnitTests.Pages.Services.JsonFileLocationTypeService
