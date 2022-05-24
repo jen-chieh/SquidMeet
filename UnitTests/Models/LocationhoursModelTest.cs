673a67447c465808c345e424922f7f529b98813b
Selina Hu <Selinaehu@gmail.com>
2022-05-23 19:21:28 -0700

    Removed unused "using" statements in LocationhoursModelTests.cs.

--- UnitTests/Models/LocationhoursModelTest.cs
+++ UnitTests/Models/LocationhoursModelTest.cs
@@ -1,9 +1,5 @@
-﻿using System.Diagnostics;
-using Microsoft.Extensions.Logging;
-using NUnit.Framework;
-using Moq;
+﻿using NUnit.Framework;
 using ContosoCrafts.WebSite.Models;
-
 
 namespace UnitTests.Models
 {
