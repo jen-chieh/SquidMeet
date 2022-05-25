d44f0bd6dd5dd8a9b04bbd70e41a4be64d35ef84
Tummala Kalyani <tkalyani@seattleu.edu>
2022-05-24 21:49:56 -0700

    Modified code to redirect to home page in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -53,7 +53,7 @@ namespace ContosoCrafts.WebSite
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePagesWithRedirects("/");
             }
             else
             {
