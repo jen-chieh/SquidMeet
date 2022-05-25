7bbb6af44dd091a1d1ebe92c1038b0140f2e353d
Selina Hu <Selinaehu@gmail.com>
2022-05-24 23:18:49 -0700

    Removed unused code in Startup.cs.

--- src/Startup.cs
+++ src/Startup.cs
@@ -58,9 +58,7 @@ namespace ContosoCrafts.WebSite
             else
             {
                 app.UseStatusCodePagesWithRedirects("/");
-                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
