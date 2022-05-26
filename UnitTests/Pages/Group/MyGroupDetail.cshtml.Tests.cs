1b76294827e21ef258e65d285a88d91728a09eb6
Selina Hu <Selinaehu@gmail.com>
2022-05-26 15:34:25 -0700

    Added 100% code coverage to MyGroupDetail.cshtml.Tests.cs.

---
+++ UnitTests/Pages/Group/MyGroupDetail.cshtml.Tests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using SquidMeet.WebSite.Pages.Group;
+
+namespace UnitTests.Pages.Group.Update
+{
+    /// <summary>
+    /// Unit Tests for onget method for MyGroupDetail model
+    /// </summary>
+    public class MyGroupDetailModelTests
+    {
+        #region TestSetup
+
+        // Data middle tier
+        public static MyGroupDetailModel pageModel;
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="meetupService"></param>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new MyGroupDetailModel(TestHelper.MeetupService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// Test to verify OnGet returns correct data with a given id and model state is valid
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("ebf80f1c-3802-4df9-bbda-fd274810a44c");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Meditate with a Monk", pageModel.Meetup.Title);
+        }
+        #endregion OnGet
+
+    }
+}
