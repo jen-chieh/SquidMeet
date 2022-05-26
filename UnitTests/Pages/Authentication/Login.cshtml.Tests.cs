c975d0c5d9e715be4c5a1e16b9e12274c51da52b
Selina Hu <Selinaehu@gmail.com>
2022-05-26 14:13:43 -0700

    Moved Login.cshtml.Tests.cs to the authentication folder.

---
+++ UnitTests/Pages/Authentication/Login.cshtml.Tests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace UnitTests.Pages.Product.Login
+{
+    /// <summary>
+    /// Test for login model with valid and invalid onPost calls
+    /// </summary>
+    public class LoginTests
+    {
+        #region TestSetup
+
+        // Data middle tier
+        public static LoginModel pageModel;
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new LoginModel(TestHelper.UserService)
+            {
+            };
+
+        }
+
+        #endregion TestSetup
+
+        #region OnPost
+
+        /// <summary>
+        /// Test to verify OnPost update meetup with valid values keeps valid model state
+        /// </summary>
+        [Test]
+        public void OnPost_Valid_Should_Return_Users()
+        {
+
+            // Arrange
+            pageModel.User = new UserModel
+            {
+                user_id = "22",
+                email = "username@squid.com",
+                password = "password",
+                name = "test name",
+                age = 30,
+                gender = "female",
+                bio = "hello"
+            };
+            // Act
+            var heetpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(heetpContext,Mock.Of<ITempDataProvider>());
+            tempData["user"] = "user";
+            pageModel.TempData = tempData;
+            var result = pageModel.OnPost(pageModel.User) as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Profile"));
+        }
+
+        [Test]
+
+        /// <summary>
+        /// Test to verify OnPost update meetup with invalid values keeps invalid model state
+        /// </summary>
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.User = new UserModel
+            {
+                user_id = "bogus",
+                email = "bogus",
+                password = "bogus",
+                name = "bogus",
+                age = 30,
+                gender = "bogus",
+                bio = "bogus"
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost(pageModel.User) as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnPost
+    }
+}
