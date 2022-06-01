ae729f0d9d42c8070e684e94637a4d39c5298781
Selina Hu <Selinaehu@gmail.com>
2022-05-31 20:21:54 -0700

    Updated onPost method unit tests in AcceptInvite.cshtml.Tests.cs.

--- UnitTests/Pages/Group/AcceptInvite.cshtml.Tests.cs
+++ UnitTests/Pages/Group/AcceptInvite.cshtml.Tests.cs
@@ -66,7 +66,7 @@ namespace UnitTests.Pages.Group.Update
                 Title = "Intro to Javascript",
                 Date = "11/22/12",
                 Attendees = new [] { "Jessica, Bob" },
-                Host  = "YJ",
+                Host  = "Blanchard Whitehead",
                 Description = "JavaScript is among the most powerful and flexible programming languages of the web. It powers the dynamic behavior on most websites. It is one of the most widely used programming languages (Front-end as well as Back-end) and it has its presence in almost every area of software development. This Workshop is specifically designed to build your JavaScript skills from scratch, we will cover JavaScript\u0027s key fundamental features.",
                 InviteCode = "e0e940ed-5487-46f9-b0f0-99d64853e2c4",
                 Img = "https://raw.githubusercontent.com/youjin6/picbed2/main/code/img/store/ok20220506135934.jpeg",
