593e30c704670948e321654c6be59de0e5044dc7
Tummala Kalyani <tkalyani@seattleu.edu>
2022-06-05 22:21:11 -0700

    Added unit test for 100% code coverage in Detail.cshtml.Tests.cs

--- UnitTests/Pages/Group/Detail.cshtml.Tests.cs
+++ UnitTests/Pages/Group/Detail.cshtml.Tests.cs
@@ -1,4 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
 using SquidMeet.WebSite.Pages.Group;
 
 namespace UnitTests.Pages.Group.Update
@@ -46,5 +52,56 @@ namespace UnitTests.Pages.Group.Update
         }
         #endregion OnGet
 
+        #region OnPostAsync
+
+        /// <summary>
+        /// Test to verify OnPost update meetup with valid values keeps valid model state
+        /// </summary>
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_ViewGroups()
+        {
+            // Arrange
+
+            // Initialize new StatusModel
+            StatusModel status = new StatusModel
+            {
+                user = "Jack",
+                status = "pending"
+            };
+
+            // Add status model to list
+            List<StatusModel> statusList = new List<StatusModel>();
+            statusList.Add(status);
+
+            pageModel.Meetup = new MeetupModel
+            {
+                meetup_id = "ebf80f1c-3802-4df9-bbda-fd274810a44c",
+                location = "Seattle",
+                LocationType = "Indoor",
+                Title = "Intro to Javascript",
+                Date = "11/22/12",
+                Attendees = statusList,
+                Host = "Blanchard Whitehead",
+                Description = "JavaScript is among the most powerful and flexible programming languages of the web. It powers the dynamic behavior on most websites. It is one of the most widely used programming languages (Front-end as well as Back-end) and it has its presence in almost every area of software development. This Workshop is specifically designed to build your JavaScript skills from scratch, we will cover JavaScript\u0027s key fundamental features.",
+                InviteCode = "e0e940ed-5487-46f9-b0f0-99d64853e2c4",
+                Img = "https://raw.githubusercontent.com/youjin6/picbed2/main/code/img/store/ok20220506135934.jpeg",
+                Video = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/33372b34-4d33-404b-a296-59691e1af211/pexels-mikhail"
+            };
+
+            // Act
+
+            var heetpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(heetpContext, Mock.Of<ITempDataProvider>());
+            tempData["user"] = "user";
+            pageModel.TempData = tempData;
+            var result = pageModel.OnPost(pageModel.Meetup) as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("ViewMygroup"));
+        }
+
+      
+        #endregion OnPostAsync
     }
 }
